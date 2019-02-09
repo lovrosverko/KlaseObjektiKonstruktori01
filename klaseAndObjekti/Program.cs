df9c8026dca138c113fa3bb9327050123105ac47
Lovro Šverko <lovro.sverko@sser.hr>
2019-02-09 14:20:46 +0100

    Add files via upload

---
+++ klaseAndObjekti/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using klaseAndObjekti.Klase;
+
+namespace klaseAndObjekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vozilo avijon = new Vozilo();
+
+            avijon.setBrojKotaca(6);
+            avijon.setLeti(true);
+            avijon.setPliva(false);
+            avijon.setVozi(true);
+            avijon.setVrsta("Cessna");
+            avijon.setOznaka("1312");
+
+            Vozilo kamijon = new Vozilo(8, false, false, true, "TAM", "RI1987AR");
+
+            Console.WriteLine("Vrsta: " + avijon.getVrsta() + " Oznaka: " +
+                avijon.getOznaka() + "\nLeti: " + avijon.getLeti() + "\nPlovi: "+ avijon.getPliva()
+                + "\nVozi: " + avijon.getVozi() + "\nBroj kotača: "+avijon.getBrojKotaca() + "\n");
+            
+            Console.WriteLine(kamijon.ToString());
+
+            Console.WriteLine(avijon.ispis());
+
+            Console.ReadLine();
+        }
+    }
+
+    
+}
